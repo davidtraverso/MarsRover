212d726aefe382775677884e1541405069d5bde0
davidtraverso <davidtraverso88@gmail.com>
2019-09-20 19:17:16 -0500

    heroku push

--- marsrover/src/Components/Landing.js
+++ marsrover/src/Components/Landing.js
@@ -4,41 +4,68 @@ class Landing extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      solRange: ['---', '2530', '2208', '5111'],
       solSelection: 0,
+      solMax: 0,
       cameraSelection: '',
-      HTTPRequest: ''
+      rover: '',
+      HTTPRequest: '',
+      isReady: true
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.validateButton = this.validateButton.bind(this);
   }
 
+  // Range input
   handleChange = e => {
     e.preventDefault();
     this.setState({
       solSelection: e.target.value
     });
+    this.validateButton();
   };
 
+  // Select input
   handleSelect = e => {
     e.preventDefault();
     this.setState({
-      cameraSelection: e.target.value
+      [e.target.name]: e.target.value
     });
+    this.validateButton();
   };
 
+  // Make the button clickable IFF all selection criteria has been made.
+  validateButton = () => {
+    if (this.state.cameraSelection !== '' && this.state.rover !== '' && this.state.solSelection !== '') {
+      this.setState({
+        isReady: false
+      });
+      console.log('Complete form');
+    } else {
+      console.log('Incomplete form');
+    }
+  };
+
+  // HTTP Request on button click
   handleClick = () => {
+    let trimIndex = this.state.cameraSelection.indexOf('|');
     this.setState({
       HTTPRequest:
-        'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=' +
+        'https://api.nasa.gov/mars-photos/api/v1/rovers/' +
+        this.state.rover +
+        '/photos?sol=' +
         this.state.solSelection +
         '&api_key=7bmHdyZcr9I0s5h0ZU02R58oeCkEu3EjmkZSKdX8&camera=' +
-        this.state.cameraSelection
+        this.state.cameraSelection.slice(0, trimIndex - 1)
     });
   };
 
   render() {
+    var rovers = ['Curiosity', 'Spirit', 'Opportunity'];
+    var solRange = 0;
     var cameras = [
       '--- Select a camera ---',
       'FHAZ | Front Hazard Avoidance Camera',
@@ -48,33 +75,42 @@ class Landing extends Component {
       'MAHLI | Mars Hand Lens Imager',
       'MARDI | Mars Descent Imager',
       'NAVCAM | Navigation Camera',
-      'PANCAM |Panoramic Camera'
+      'PANCAM | Panoramic Camera'
     ];
     return (
       <div>
         <h1>This is the landing page!</h1>
         <hr />
         <h3>Query criteria</h3>
-        <p>Sol selection (Mars day): {this.state.solSelection}</p>
-        <p>Camera selection: {this.state.cameraSelection}</p>
-        <form>
-          <div className="form-group">
-            0 <input type="range" min="0" max="1000" onChange={this.handleChange} /> 1000
-            <br />
-            <select id="pet-select" onChange={this.handleSelect}>
-              {cameras.map((camera, i) => {
-                return (
-                  <option key={i} value={camera}>
-                    {camera}
-                  </option>
-                );
-              })}
-            </select>
-            <br />
-            <input type="button" value="Show me Mars!" onClick={this.handleClick} />
-            <p>{this.state.HTTPRequest}</p>
-          </div>
-        </form>
+        <select id="roverSelect" name="rover" onChange={this.handleSelect}>
+          {rovers.map((rover, i) => {
+            return (
+              <option key={i} value={rover}>
+                {rover}
+              </option>
+            );
+          })}
+        </select>
+        <br />
+        <br />
+        <p>Sol selection: {this.state.solSelection}</p>
+        0 <input type="range" min="0" max={this.state.solRange[2]} onChange={this.handleChange} />{' '}
+        {this.state.solRange[solRange]}
+        <br />
+        <br />
+        <select id="cameraSelect" name="cameraSelection" onChange={this.handleSelect}>
+          {cameras.map((camera, i) => {
+            return (
+              <option key={i} value={camera}>
+                {camera}
+              </option>
+            );
+          })}
+        </select>
+        <br />
+        <br />
+        <input type="button" value="Show me Mars!" disabled={this.state.isReady} onClick={this.handleClick} />
+        <p>{this.state.HTTPRequest}</p>
       </div>
     );
   }
